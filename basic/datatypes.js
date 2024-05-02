ff179408e6c9a9fb3c80d4d951e61773ae4f4a6f
Mr.Beast <112603713+Amar-3@users.noreply.github.com>
2024-05-02 11:20:07 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"  // treate all js code as newer version
+
+// alert(2+2);  we are using node.js not browser thats why alsert is written in different b=manner
+
+
+// datatypes 
+// string 
+//number2^53
+//bool true/false
+//bigint 
+// null its a standalone value khalihai
+// symbol  for unique
+
+
+// object
+
+
+console.log(typeof (null));
+console.log(typeof (undefined));
+
---
+++ basic/variables.js
@@ -0,0 +1,15 @@
+const accountId =142324   // not changable
+let accEmail="efjskjdfbksjbsdkjsdkf@smdvjs"      // changable and use in js inplac eof var
+
+// javascript have no  concept of scope.
+
+var accountPass = "12345" // confusin of scope concept.  becaue of block and functional scope
+// var let const ye dikhe compiler me memory create karo aur memory me store karo
+
+accCity="san farnsisco"  
+//accountId=4
+// const one defines would not change for entire program
+
+console.log(accCity);
+
+console.table([accountId, accountPass,accEmail]);
