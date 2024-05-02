f3057571a390f61aa5f782b03aa9ff1acafaa1b2
Mr.Beast <112603713+Amar-3@users.noreply.github.com>
2024-05-02 11:46:23 +0000

    basic/arithmatic.js

---
+++ basic/arithmatic.js
@@ -0,0 +1,37 @@
+console.table([2+2 , 2-2 , 2/2 , 2**2, 2**53 , 2%3]);
+
+let val = 33;
+let negval=-val;
+
+console.table([val,negval]);
+
+
+// string number addition conversion
+
+console.log("1  " + 2)
+console.log("1  " + 2 +2)
+console.log("1  " +" 2")
+console.log(1 + "  2")
+console.log("all priorir numbers are added first than merged with string 1+1+2 = 4 + '5' = 45 ")
+
+console.log(1+1 + 2+"5");
+
+
+
+console.log(+true)
+
+
+let num1,num2, num3;
+
+num1=num2=num3=45 // samrt bu in production we focus on code resuability
+console.log([num1,num2,num3]);
+
+
+let counter=23
+
+console.log(counter++)
+console.log(counter)
+console.log(++counter)
+
+
+
