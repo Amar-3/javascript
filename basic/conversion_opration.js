91c2f702eaf6b04e690d92283c141b8b68c80e6d
Mr.Beast <112603713+Amar-3@users.noreply.github.com>
2024-05-02 11:32:02 +0000

    basic/conversion_opration.js

---
+++ basic/conversion_opration.js
@@ -0,0 +1,16 @@
+// helps to transform type of data fie over internet since in cables transfersin different formates
+
+let  num="33";
+let score="33abc"
+let value=Number(num);
+
+let val=1
+let boole = Boolean(val);
+
+let intostring= String(val);
+
+console.table([score,value,boole , val , intostring]);
+
+
+// number of string in converted in number
+// number mixed string will not be converted  response is NaN not a number
